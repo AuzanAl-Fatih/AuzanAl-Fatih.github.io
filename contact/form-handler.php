15a249d4d91cb760d0962b99bb4b221defaaa674
AuzanAl-Fatih <ahmad.14117007@student.itera.ac.id>
2019-10-11 10:11:39 +0700

    edit sikit

--- contact/form-handler.php
+++ contact/form-handler.php
@@ -2,7 +2,7 @@
 include('SMTPClass.php');
 
 $use_smtp = '0';
-$emailto = 'yourname@yourdomain.com';
+$emailto = 'ahmad.14117007@student.itera.com';
 
 	// retrieve from parameters
 	$emailfrom = isset($_POST["email"]) ? $_POST["email"] : "";
